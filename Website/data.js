1c23b72d6cc4995151c7fcee78c5abef281d17aa
KhoaDang2011 <chautrandangkhoa2011@gmail.com>
2026-03-19 10:28:52 +0700

    Add files via upload

---
+++ Website/data.js
@@ -0,0 +1,56 @@
+const videos = [
+    {
+        id: 'v1',
+        title: 'Trải nghiệm MacBook Air M3 - Có thực sự "Pro"?',
+        thumbnail: 'thumb1.jpg',
+        channelName: 'Tinh Tế',
+        views: '1.5Tr',
+        time: '2 ngày trước',
+        duration: '18:30'
+    },
+    {
+        id: 'v2',
+        title: 'Thử thách 24h sống trong rừng không có gì',
+        thumbnail: 'thumb2.jpg',
+        channelName: 'Survival VN',
+        views: '3.2Tr',
+        time: '1 tuần trước',
+        duration: '45:12'
+    },
+    {
+        id: 'v3',
+        title: 'Công thức nấu Phở Bò gia truyền Hà Nội',
+        thumbnail: 'thumb3.jpg',
+        channelName: 'Bếp Của Mẹ',
+        views: '980N',
+        time: '3 ngày trước',
+        duration: '15:22'
+    },
+    {
+        id: 'v4',
+        title: 'Tổng hợp những bàn thắng đẹp nhất của Messi',
+        thumbnail: 'thumb4.jpg',
+        channelName: 'Kênh Bóng Đá',
+        views: '10.5Tr',
+        time: '1 năm trước',
+        duration: '09:45'
+    },
+    {
+        id: 'v5',
+        title: 'Làm thế nào để học một ngôn ngữ mới hiệu quả?',
+        thumbnail: 'thumb5.jpg',
+        channelName: 'Gemini Learning',
+        views: '750N',
+        time: '2 tháng trước',
+        duration: '22:10'
+    },
+    {
+        id: 'v6',
+        title: 'Nhạc Lofi giúp bạn tập trung làm việc 100%',
+        thumbnail: 'thumb6.jpg',
+        channelName: 'The Lofi Garden',
+        views: '8.1Tr',
+        time: '1 năm trước',
+        duration: '3:30:00'
+    }
+];
